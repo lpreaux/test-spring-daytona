a043bc80f8b5cbb98a46fcabea75d84cda121a18
Lucas Préaux <contact@lucaspreaux.fr>
2024-05-24 16:58:22 +0200

    :tada: feat: Initial commit

---
+++ src/main/java/fr/lpreaux/testspringdaytona/TestSpringDaytonaApplication.java
@@ -0,0 +1,13 @@
+package fr.lpreaux.testspringdaytona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringDaytonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringDaytonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/lpreaux/testspringdaytona/TestSpringDaytonaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lpreaux.testspringdaytona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringDaytonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
